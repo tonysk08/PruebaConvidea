23408df924ad4e76083fb4c0cb0d844a4c1c4f7e
Antonio Sarmiento <zarmiento227@gmail.com>
2021-03-07 16:01:29 -0500

    admin part for add & delet

--- controllers/CarsController.js
+++ controllers/CarsController.js
@@ -7,12 +7,54 @@ controller.list = (req, res) => {
             {
                 res.json(err);
             }
-            console.log(automobiles)
             res.render('automobiles',{
                 data:automobiles
             })
         });
     });
+};
+
+//opccion para administrar y ver la lista de coches
+controller.adminCars = (req,res) => {
+    req.getConnection((err,conn) => {
+        conn.query('SELECT * FROM automobiles', (err,automobiles) =>{
+            if(err)
+            {
+                res.json(err);
+            }
+            //console.log(automobiles);
+            res.render('admincars',{
+                data:automobiles
+            })
+        });
+    });
+};
+
+controller.addCar = (req,res) =>{
+    res.render('addcar');
+}
+
+//Agregar un auto nuevo
+controller.saveCar = (req,res) =>{
+    const data = req.body;
+    console.log(data)
+    req.getConnection((err,conn) => {
+        conn.query('INSERT INTO automobiles set ?',[data], (err,automoviles)=>{
+            console.log(automoviles);
+            res.redirect('/cars/admincars');
+        });
+    })
+};
+
+//eliminacion de un auto 
+controller.deleteCar = (req,res) =>{
+    
+    const id = req.params.id;
+    req.getConnection((err,conn) => {
+        conn.query('DELETE FROM automobiles WHERE id = ?',[id], (err, rows) =>{
+            res.redirect('/cars/adminCars');
+        });
+    })
 };
 
 module.exports = controller
--- routes/cars.js
+++ routes/cars.js
@@ -6,5 +6,10 @@ const CarController = require('../controllers/CarsController');
 
 /* GET lista todos los carros disponibles*/
 router.get('/', CarController.list);
+router.get('/admincars', CarController.adminCars);
+router.get('/delete/:id', CarController.deleteCar);
+router.get('/add', CarController.addCar);
+router.post('/save', CarController.saveCar);
+
 
 module.exports = router;
